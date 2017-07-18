42eed020c8c45340ed62c91d22f490e5b2c5b9b6
Hzzone <Hzzone@outlook.com>
2017-07-18 09:55:06 +0800

    注册完成，验证码功能未完成

--- Database.py
+++ Database.py
@@ -11,31 +11,29 @@ def isUserExistsOrPasswdCorrect(user_id, password, cursor):
     cursor.execute("select * from public.user where user_id=%s", [user_id])
     rows = cursor.fetchall()
     if(len(rows)==0):
-        print("login: user is not exists")
-        return ReturnValue().toString()
+        return ReturnValue(msg='login: user does not exist').toString()
     else:
         one = rows[0]
         user = User(user_id=one[0], phone_number=one[1], user_name=one[2], password=one[3], ismale=[4])
         if user.password != password:
-            print("password is not correct")
-            return ReturnValue(isOk=False, user=user).toString()
+            return ReturnValue(isOk=False, msg='password is not correct', user=user).toString()
         else:
-            print("login success")
-            return ReturnValue(isOk=True, user=user).toString()
+            return ReturnValue(isOk=True,msg='login success', user=user).toString()
 
 # 判断用户是否已经存在，不存在则插入
-def isUserExistsOrRegisterSuccess(user, cursor):
+def isUserExistsOrRegisterSuccess(user, authcode=None, conn=None):
+    cursor = conn.cursor()
     cursor.execute("select * from public.user where user_id=%s", [user.user_id])
     rows = cursor.fetchall()
     if (len(rows) == 0):
-        print("register: user is not exists")
-        cursor.execute("insert into public.user value(%s, %s, %s, %s, %s", [user.user_id, user.phone_number,
-                                                                user.user_name, user.password, user.ismale])
-        return ReturnValue(isOk=True, msg='register', user=user).toString()
+        cursor.execute("insert into public.user values(%s, %s, %s, %s, %s)", [user.user_id, user.phone_number, user.user_name, user.password, 'True' if user.ismale else 'False'])
+        conn.commit()
+        return ReturnValue(isOk=True, msg='register success', user=user).toString()
     else:
-        return ReturnValue(isOk=False, msg='user exists', user=user).toString()
+        return ReturnValue(isOk=False, msg='register: user exists', user=user).toString()
 
 
 conn = connectPostgreSQL()
-# print(isUserExistsOrPasswdCorrect("zhizhonghwang@gmail.com", "111", conn.cursor()))
-print(isUserExistsOrRegisterSuccess(User(user_id='11@qq.com')), cursor=conn.cursor())
+# print(isUserExistsOrPasswdCorrect("11@qq.com", "1111", conn.cursor()))
+user = User(user_id='11@qq.com', password='1111')
+print(isUserExistsOrRegisterSuccess(user, conn=conn))
--- Run4Fun-Server.py
+++ Run4Fun-Server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from Database import *
 
 app = Flask(__name__)
@@ -14,13 +14,16 @@ def login():
     user_passwd = request.form.get('password')
     print("id: "+user_id)
     print("password: "+user_passwd)
-    return isUserExistsOrPasswdCorrect(user_id=user_id, user_passwd=user_passwd, cursor=conn.cursor())
+    return isUserExistsOrPasswdCorrect(user_id=user_id, password=user_passwd, cursor=conn.cursor())
 
 @app.route('/register', methods=['POST'])
 def register():
     user_id = request.form.get('user_id')
     user_passwd = request.form.get('password')
-
+    phone_number = request.form.get('phone_number')
+    user_name = request.form.get('user_name')
+    ismale = request.form.get('ismale')
+    return isUserExistsOrRegisterSuccess(User(user_id=user_id, phone_number=phone_number, user_name=user_name, password=user_passwd, ismale=ismale), conn=conn)
 
 # 数据库连接
 global conn
